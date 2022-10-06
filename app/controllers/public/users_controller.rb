2353182d3114be02943d991e5270c77f6aad59ef
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-06 10:13:42 +0000

    [add]会員情報編集ページ作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,7 +6,18 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
 
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_mypage_path
+  end
+
+  private
+  def user_params
+    params.require(:user). permit(:nickname, :introduction, :profile_image)
   end
 
 end
