11e74c79e0791c428dca65648eedd591b62f7fd0
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-10 03:24:53 +0000

    [add]いいね機能非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,12 +3,12 @@ class Public::FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(user_id: current_user.id, outfit_id: params[:outfit_id])
     @favorite.save
-    redirect_back(fallback_location: root_path)
+    @outfit = Outfit.find(params[:outfit_id])
   end
 
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, outfit_id: params[:outfit_id])
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @outfit = Outfit.find(params[:outfit_id])
   end
 end
