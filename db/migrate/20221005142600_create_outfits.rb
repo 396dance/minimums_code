87797d1e8cf30490eacbd0b05b8b33fd31a8f53c
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-15 10:02:36 +0000

    [update]マイグレーションファイルのインデントを修正

--- db/migrate/20221005142600_create_outfits.rb
+++ db/migrate/20221005142600_create_outfits.rb
@@ -2,8 +2,8 @@ class CreateOutfits < ActiveRecord::Migration[6.1]
   def change
     create_table :outfits do |t|
       t.integer :user_id, null: false
-      t.string  :title,   null: false
-      t.text    :body,    null: false
+      t.string  :title, null: false
+      t.text    :body, null: false
       t.timestamps
     end
   end
--- db/migrate/20221006045848_create_outfit_comments.rb
+++ db/migrate/20221006045848_create_outfit_comments.rb
@@ -1,9 +1,9 @@
 class CreateOutfitComments < ActiveRecord::Migration[6.1]
   def change
     create_table :outfit_comments do |t|
-      t.integer   :user_id
-      t.integer   :outfit_id
-      t.text      :comment
+      t.integer :user_id
+      t.integer :outfit_id
+      t.text :comment
       t.timestamps
     end
   end
