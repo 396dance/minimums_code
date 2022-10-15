56e22c2b302c437a299ac7283eedb50d0d307cc9
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-15 08:23:28 +0000

    [fix]テストデプロイでエラー発生のためuserマイグレーションファイルの記述修正

--- db/migrate/20221005142600_create_outfits.rb
+++ db/migrate/20221005142600_create_outfits.rb
@@ -1,9 +1,9 @@
 class CreateOutfits < ActiveRecord::Migration[6.1]
   def change
     create_table :outfits do |t|
-      t.integer :user_id, null: false, default: ""
-      t.string  :title,   null: false, default: ""
-      t.text    :body,    null: false, default: ""
+      t.integer :user_id, null: false
+      t.string  :title,   null: false
+      t.text    :body,    null: false
       t.timestamps
     end
   end
