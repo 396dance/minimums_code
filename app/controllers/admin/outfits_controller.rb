947e0b7aa1eae7e53ae6c2d3ac90cd8d498c6088
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-18 13:28:43 +0000

    [fix]Aboutページオーバーフロー修正

--- app/controllers/admin/outfits_controller.rb
+++ app/controllers/admin/outfits_controller.rb
@@ -1,19 +1,26 @@
 class Admin::OutfitsController < ApplicationController
    # adminがログインしていないとページ見れない
   before_action :authenticate_admin!
+  before_action :outfit_find, only: [:show, :destroy]
 
   def index
     @outfits = Outfit.all
   end
 
   def show
-    @outfit = Outfit.find(params[:id])
+    # @outfit = Outfit.find(params[:id])
     @outfit_comment = OutfitComment.new
   end
 
   def destroy
-    @outfit = Outfit.find(params[:id])
+    # @outfit = Outfit.find(params[:id])
     @outfit.destroy
     redirect_to admin_outfits_path
+  end
+  
+  private
+  
+  def outfit_find
+    @outfit = Outfit.find(params[:id])
   end
 end
