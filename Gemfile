da006ec2e29f97093ef5609b3aac52830d8cee3b
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-22 09:43:14 +0000

    [add]ページネーションデザイン変更

--- Gemfile
+++ Gemfile
@@ -69,4 +69,5 @@ gem 'pry-rails'
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
-gem 'kaminari','~> 1.2.1'
+gem 'kaminari'
+
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -7,13 +7,9 @@ class Public::SearchesController < ApplicationController
 
     if @range == "ニックネーム"
       @users = User.looks(params[:search], params[:word])
-      # @users.include?
-      # render search_path
     else
       @outfits = Outfit.looks(params[:search], params[:word])
     end
   end
 
 end
-
-# [{id:1 name: test}.{id:2 name: sample}.c.d.e.f]
