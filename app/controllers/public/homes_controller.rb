a50d5fd30b1880163b1b452eea29aeda33657c6b
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-22 04:43:05 +0000

    [fix]ランキング機能スペルミス修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @all_ranks = Outfit.find(Favorite.group(:oufit_id).order('count(outfit_id) desc').limit(5).pluck(:outfit_id))
+    @all_ranks = Outfit.find(Favorite.group(:outfit_id).order('count(outfit_id) desc').limit(5).pluck(:outfit_id))
   end
 
 end
