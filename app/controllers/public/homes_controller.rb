6fd728df3aebe29abb4c6622561aa269b6bafb4a
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-23 06:53:05 +0000

    [update]トップページレイアウト修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
 
   def top
     @all_ranks = Outfit.find(Favorite.group(:outfit_id).order('count(outfit_id) desc').limit(5).pluck(:outfit_id))
+    @tag = Tag.all
   end
 
 end
