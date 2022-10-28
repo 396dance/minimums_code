508277980648fa37a16b03455fdc26752a17226e
yasuhiromikuno <mikuno-0112@outlook.jp>
2022-10-28 09:02:05 +0000

    [update]会員側レイアウト修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :outfit_tags, dependent: :destroy
-  has_many :outfits, through: :outfit_tags
+  has_many :outfits, through: :outfit_tags, dependent: :destroy
 end
